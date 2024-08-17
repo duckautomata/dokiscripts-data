11f74353f8abb16aabdcd571dd605538f53e2107
duckautomata <159738465+duckautomata@users.noreply.github.com>
2024-08-17 14:28:18 -0500

    Updated transcripts, uptated pfp,  automated twitch transcripts

--- Cleanup-Audio.ps1
+++ Cleanup-Audio.ps1
@@ -3,7 +3,7 @@ $confirmation = Read-Host "Are you sure you want to delete all audio files in Tr
 
 if ($confirmation -eq "Y" -or $confirmation -eq "y") {
     $i = 0
-    Get-ChildItem -Path ".\Transcript" -Include *.webm, *.m4a, *.mp3, *.mp4 -File -Recurse | ForEach-Object {
+    Get-ChildItem -Path ".\Transcript" -Include *.webm, *.m4a, *.mp3, *.mp4, *.mkv -File -Recurse | ForEach-Object {
         $i += 1
         $_.Delete()
     }
