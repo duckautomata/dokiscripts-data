39372578cc12985f8b1c1c0182bfa0dbe78d9af6
duckautomata <159738465+duckautomata@users.noreply.github.com>
2024-09-03 16:58:43 -0500

    Update Transcript 24-09-02

--- Download-Audio.ps1
+++ Download-Audio.ps1
@@ -12,47 +12,44 @@ function Get-Audio {
         [string]$Channel
     )
 
-    .\yt-dlp.exe `
-        --download-archive yt-dlp-archive-transcript.txt `
-        --cookies-from-browser firefox `
-        --ignore-errors `
-        --match-filter !is_live `
-        --write-thumbnail `
-        -f 'ba' `
-        -o "Transcript/$Channel/%(upload_date)s - $Type - %(title)s - [%(id)s].%(ext)s" `
-        --windows-filenames `
-        --sleep-requests 1 `
-        --sleep-interval 15 `
-        $URL
-}
-
-function Get-TwitchAudio {
-    param (
-        [string]$URL,
-        [string]$Type,
-        [string]$Channel
-    )
-
-    .\yt-dlp.exe `
-        --download-archive yt-dlp-archive-transcript.txt `
-        --cookies-from-browser firefox `
-        --ignore-errors `
-        --match-filter !is_live `
-        -f 'ba' `
-        -o "Transcript/$Channel/%(upload_date)s - $Type - %(title)s - [%(id)s].%(ext)s" `
-        --windows-filenames `
-        --sleep-requests 1 `
-        --sleep-interval 15 `
-        $URL
+    # If we are downloading from twitch, we don't want to save the thumbnail
+    if ($URL.ToLower().Contains("twitch.tv")) {
+        Write-Host "Downloading from twitch, not including thumbnail"
+        .\yt-dlp.exe `
+            --download-archive yt-dlp-archive-transcript.txt `
+            --cookies-from-browser firefox `
+            --ignore-errors `
+            --match-filter !is_live `
+            -f 'ba' `
+            -o "Transcript/$Channel/%(upload_date)s - $Type - %(title)s - [%(id)s].%(ext)s" `
+            --windows-filenames `
+            --sleep-requests 1 `
+            --sleep-interval 15 `
+            $URL
+    } else {
+        .\yt-dlp.exe `
+            --download-archive yt-dlp-archive-transcript.txt `
+            --cookies-from-browser firefox `
+            --ignore-errors `
+            --match-filter !is_live `
+            --write-thumbnail `
+            -f 'ba' `
+            -o "Transcript/$Channel/%(upload_date)s - $Type - %(title)s - [%(id)s].%(ext)s" `
+            --windows-filenames `
+            --sleep-requests 1 `
+            --sleep-interval 15 `
+            $URL
+    }
 }
 
 .\yt-dlp.exe -U
 Get-Audio -Channel "Dokibird" -URL "https://www.youtube.com/Dokibird/membership" -Type "Members"
 Get-Audio -Channel "Dokibird" -URL "https://www.youtube.com/Dokibird/videos" -Type "Video"
 Get-Audio -Channel "Dokibird" -URL "https://www.youtube.com/@DokibirdVODs/videos" -Type "TwitchVod"
-Get-TwitchAudio -Channel "Dokibird" -URL "https://www.twitch.tv/dokibird/videos?filter=archives&sort=time" -Type "Twitch"
+Get-Audio -Channel "Dokibird" -URL "https://www.twitch.tv/dokibird/videos?filter=archives&sort=time" -Type "Twitch"
 Get-Audio -Channel "Dokibird" -URL "https://www.youtube.com/Dokibird/streams" -Type "Stream"
 Get-Audio -Channel "MintFantome" -URL "https://www.youtube.com/@mintfantome/membership" -Type "Members"
 Get-Audio -Channel "MintFantome" -URL "https://www.youtube.com/@mintfantome/videos" -Type "Video"
 Get-Audio -Channel "MintFantome" -URL "https://www.youtube.com/@mintfantome/streams" -Type "Stream"
-# Get-Audio -Channel "Dokibird" -URL "https://www.youtube.com/live/WNiCnSHK3Gc" -Type "External"
+Get-Audio -Channel "Dokibird" -URL "https://www.twitch.tv/videos/2240830475" -Type "External"
+Get-Audio -Channel "Dokibird" -URL "https://www.twitch.tv/videos/2240305592" -Type "External"
