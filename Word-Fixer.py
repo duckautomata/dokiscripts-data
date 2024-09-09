4ce26b0421bab7a0ef935ec3090e3ff02b3a92b2
duckautomata <159738465+duckautomata@users.noreply.github.com>
2024-09-08 23:38:36 -0500

    Update Transcript 24-09-08

--- Word-Fixer.py
+++ Word-Fixer.py
@@ -13,14 +13,14 @@ def replace_words_in_srt_files(word_map: dict[str,str], directory: str):
         if file.endswith('.srt'):
           file_path = os.path.join(root, file)
           
-          with open(file_path, 'r', encoding='utf-8') as f:
+          with open(file_path, 'r+', encoding='utf-8') as f:
             content = f.read()
 
-          for old_word, new_word in word_map.items():
-            content = content.replace(old_word.lower(), new_word.lower())
-            content = content.replace(old_word.capitalize(), new_word.capitalize())
+            for old_word, new_word in word_map.items():
+              content = content.replace(old_word.lower(), new_word.lower())
+              content = content.replace(old_word.capitalize(), new_word.capitalize())
 
-          with open(file_path, 'w', encoding='utf-8') as f:
+            f.seek(0)
             f.write(content)
             
           pbar.update(1)
